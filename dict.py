cbf5953668707ecbd96ccc2c1656c151f06af072
DeAndre Boston <dboston3@yahoo.com>
2020-03-31 16:32:18 -0500

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,13 @@
+# Key: State
+# Value: Capital
+
+statesToCapitals = {
+
+}
+
+
+# adding to the dictionary
+statesToCapitals["Texas"] = "Austin"
+statesToCapitals["New York"] = "Albany"
+
+print(statesToCapitals["New York"])
